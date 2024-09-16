cdb1fb4376cc6e05f640bbde6e51b5d76052bd3c
anasbm1000 <pkd19cs009@gecskp.ac.in>
2024-09-16 22:43:08 +0530

    Set routes of User profile and user details

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Registration from './components/Registration';
 import Home from './components/Home';
-// import Userhome from './components/Userhome';
-// import Userprofile from './components/Userprofile';
-// import DetailsOfUsers from './components/DetailsOfUsers';
+import Profile from './components/Profile';
+import DetailsOfUsers from './admin/DetailsOfUsers';
 import './App.css';
 
 const App = () => {
@@ -16,9 +15,8 @@ const App = () => {
         <Route path='/' Component={Login} />
         <Route path='/Home'  Component={Home} />
         <Route path='/Registration' Component={Registration} />
-        {/* <Route path='/Userhome' Component={Userhome} />
-        <Route path='/Userprofile' Component={Userprofile} />
-        <Route path='/DetailsOfUsers' Component={DetailsOfUsers} /> */}
+        <Route path='/Profile' Component={Profile} />
+        <Route path='/DetailsOfUsers' Component={DetailsOfUsers} /> 
       </Routes>
     </Router>
   );
