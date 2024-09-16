2d9f92256b5bf6f2512ba20070f6fd2e97e6b63a
anasbm1000 <pkd19cs009@gecskp.ac.in>
2024-09-16 07:38:41 +0530

    Implemented login interface of the application

---
+++ src/components/Login.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import '../App.css';
+
+const Login = () => {
+  const [state, setState] = useState({
+    username: '',
+    password: ''
+  });
+
+  const navigate = useNavigate();  
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const response = await fetch('http://localhost:3010/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(state),
+    });
+
+    const result = await response.json();
+    if (result.success) {
+      alert(result.message);
+      console.log(result.message);
+      localStorage.setItem("userId", result.userId);
+      navigate('/Home'); 
+      setState({
+        username: '',
+        password: ''
+      })
+    } else {
+      alert(result.message);
+      console.log(result.message);
+      setState({
+        username: '',
+        password: ''
+      })
+    }
+  };
+
+  return (
+    <div className="login-page">
+      <div className="form">
+        <form className="login-form" onSubmit={handleSubmit}>
+          <input type="text" placeholder="Username" value={state.username} name="username" onChange={(event) => setState({ ...state, username: event.target.value })} />
+          <input type="password" placeholder="Password" value={state.password} name="password" onChange={(event) => setState({ ...state, password: event.target.value })} />
+          <input type="submit" className="button" value="Login" />
+          <p className="message">New Customer? <Link to="/Registration">Register Here</Link></p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
