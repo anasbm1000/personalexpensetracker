ab1ac6ae21c268f244b7040883a5a53443c87782
anasbm1000 <pkd19cs009@gecskp.ac.in>
2024-09-18 06:52:13 +0530

    Added a main tag. Imported customized message feature

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ import Registration from './components/Registration';
 import Home from './components/Home';
 import Profile from './components/Profile';
 import DetailsOfUsers from './admin/DetailsOfUsers';
+import Customizedmsg from './components/Customizedmsg';
 import './App.css';
 
 const App = () => {
   
   return (
-    <Router>
-      <Routes>
-        <Route path='/' Component={Login} />
-        <Route path='/Home'  Component={Home} />
-        <Route path='/Registration' Component={Registration} />
-        <Route path='/Profile' Component={Profile} />
-        <Route path='/DetailsOfUsers' Component={DetailsOfUsers} /> 
-      </Routes>
-    </Router>
+    <main>
+      <Router>
+        <Routes>
+          <Route path='/' Component={Login} />
+          <Route path='/Home'  Component={Home} />
+          <Route path='/Registration' Component={Registration} />
+          <Route path='/Profile' Component={Profile} />
+          <Route path='/DetailsOfUsers' Component={DetailsOfUsers} /> 
+          <Route path='/Customizedmsg' Component={Customizedmsg} />
+        </Routes>
+      </Router>
+    </main>
+    
   );
 };
 
